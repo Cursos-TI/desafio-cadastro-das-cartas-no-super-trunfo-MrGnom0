b1bdbb7bb417ef1f8bb4727c45395258cecc5dc3
MrGnom0 <godiemskiribas@gmail.com>
2025-02-24 05:32:41 +0000

    alterado de string para char na linha 23 e 59

--- desafio_super_trunfo_novato.c
+++ desafio_super_trunfo_novato.c
@@ -6,7 +6,7 @@ int main(){
     
     float area1, area2 ,PIB1,PIB2;
     
-    char nome_da_cidade1[20],nome_da_cidade2[20],codigo_da_carta1[10],codigo_da_carta2[10],estado1[10], estado2[10];
+    char nome_da_cidade1[20],nome_da_cidade2[20],codigo_da_carta1[10],codigo_da_carta2[10],estado1,estado2;
 
     // PT1 E PT2 SÃO  ABREVIAÇÃO DE "PONTOS TURISTICOS"
     
@@ -19,26 +19,27 @@ int main(){
     printf("Insira o número da carta que deseja adicionar:\n");
     scanf("%d", &numero_da_carta1);
 
-    printf("Insira o estado da carta escolhendo uma letra (letras disponiveis entre 'A' a 'H'):\n");
-    scanf("%s", &estado1);
-    printf("Insira o código da carta (ESTADO + NUMERO de '1' a '4' Ex :A01 , B02....):\n");
-    scanf("%s", &codigo_da_carta1);
+    printf("Insira o estado da carta (letras disponíveis entre 'A' a 'H'):\n");
+    scanf(" %c", &estado1);
+
+    printf("Insira o código da carta (Ex: A01, B02...):\n");
+    scanf("%s", codigo_da_carta1);
 
     printf("Insira o nome da cidade desta carta: \n");
     scanf("%s", &nome_da_cidade1);
 
-    printf("Insira a população de %s\n",nome_da_cidade1);
+    printf("Insira a população de %s:\n", nome_da_cidade1);
     scanf("%d", &populacao1);
 
-
-    printf("Insira a área em km² de  %s\n",nome_da_cidade1);
+    printf("Insira a área em km² de %s:\n", nome_da_cidade1);
     scanf("%f", &area1);
 
     printf("Insira o PIB de %s\n",nome_da_cidade1);
     scanf("%f", &PIB1);
 
-    printf("Insira a quantidade de pontos túristicos de %s\n",nome_da_cidade1);
-    scanf("%d",&PT1);
+    printf("Insira a quantidade de pontos turísticos de %s:\n", nome_da_cidade1);
+    scanf("%d", &PT1);
+
     printf("\n");
     
     printf("Informações da carta inseridas com sucesso!\n");
@@ -55,7 +56,7 @@ int main(){
     scanf("%d", &numero_da_carta2);
 
     printf("Insira o estado da carta escolhendo uma letra (letras disponiveis entre 'B' a 'H'):\n");
-    scanf("%s", &estado2);
+    scanf(" %c", &estado2);
 
     printf("Insira o código da carta (ESTADO + NUMERO de '2' a '4' Ex : B02, C03...):\n");
     scanf("%s", &codigo_da_carta2);
@@ -85,7 +86,7 @@ int main(){
 
     //EXIBIÇÃO CARTA1
     printf("CARTA 1:\n");
-    printf("Estado: %s", estado1);
+    printf("Estado: %c", estado1);
     printf("\n");
     printf("Código: %s",codigo_da_carta1);
     printf("\n");
@@ -105,7 +106,7 @@ int main(){
 
     //EXIBIÇÃO CARTA2
     printf("CARTA 2:\n");
-    printf("Estado: %s", estado2);
+    printf("Estado: %c", estado2);
     printf("\n");
     printf("Código: %s",codigo_da_carta2);
     printf("\n");
@@ -113,7 +114,7 @@ int main(){
     printf("\n");
     printf("Polulação: %d", populacao2);
     printf("\n");
-    printf("Área: %.2fkm²",area2);
+    printf("Área: %.2f km²",area2);
     printf("\n");
     printf("PIB: %.2f bilhões de reais ",PIB2);
     printf("\n");
